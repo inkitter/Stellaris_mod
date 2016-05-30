af186377c70b236bc20c87a240009552cfad37ad
Inkit <inkitter@gmail.com>
2016-05-30 11:18:22 +0900

    update

--- InkitPackSel/zBeautiful Battles/common/defines/bb_defines.lua
+++ InkitPackSel/zBeautiful Battles/common/defines/bb_defines.lua
@@ -1,10 +1,16 @@
-NDefines.NCombat.COMBAT_BACK_OFF_RATE = 0.35
+NDefines.NCamera.ENTER_SYSTEM_ZOOM_STEP = 8 -- 6  Controls which zoom step is used after entering system
+NDefines.NCamera.SYSTEM_FOCUS_PLANET_STEP = 5 -- 2  Controls which zoom step to planet zoom to in a system
+NDefines.NCamera.ZOOM_STEPS_SYSTEM_PERCENTAGES = { 0.025, 0.05, 0.075, 0.1, 0.15, 0.25, 0.5, 1.0, 1.5, 3.0 } -- (0.025, 0.1, 0.25, 0.5, 1.0, 1.5, 3.0) System zoom steps, in percent relative to the system size		
+
+NDefines.NCombat.COMBAT_BACK_OFF_RATE = 0.35 -- 0.25
 NDefines.NCombat.COMBAT_DETECT_RANGE_MULT = 0.95	-- 1.05 Range to engage in combat with other ships is <fleets max attack range> * COMBAT_DETECT_RANGE_MULT
 NDefines.NCombat.FLEET_MOVE_MAX_DIST_SHIPS = 50.0	-- 10.0 Max distance that ships are allowed to travel before last ship
+
+NDefines.NGraphics.PLANET_SCALE_SYSTEM = { 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 } -- (0.425, 0.325, 0.35, 0.5, 0.5, 0.5, 0.75 ) Scale for each System zoom steps
 NDefines.NGraphics.SHIP_RANDOM_HEIGHT_OFFSET = 50.0 -- 20.0
-NDefines.NGraphics.TRAILS_MISSILE_ALPHA_FADE = 1 -- Controls of quick we alpha fade-out missile trails
-NDefines.NGraphics.TRAILS_MISSILE_BASE_WIDTH = 2.0
 NDefines.NGraphics.BALLISTIC_PROJECTILE_MISSED_LIFETIME = 5.0 -- missed ballistic projectiles will live for (at least) this long before being removed
 NDefines.NGraphics.MISSED_BEAM_LENGTH = 1000.0 -- 500.0 how long missed beams will be
+
 NDefines.NShip.FLEET_BASE_FORMATION_SCALE = 4 -- 2.0 
+
 
